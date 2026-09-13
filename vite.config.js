05b0211e55bd062ab5de48507a9708844dd4ab60
ykcnet <323605917+ykcnet@users.noreply.github.com>
2026-09-13 14:25:35 +0800

    fix: update vite config base path

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: './'
-});
+  base: '/Baseball-Team-Manager-Pro/', // 💡 確保這行路徑正確即可
+})
